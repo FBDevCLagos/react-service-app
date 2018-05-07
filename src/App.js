7e89797e415fd39ab4d5c0ae89c700bd1da43790
Ademola <ooade96@gmail.com>
2018-05-08 00:03:22 +0100

    Add Server (#23)
    * Add server code
    
    * Update README.md

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Button } from 'react-bootstrap'
+import React, {Component} from 'react'
+import {Button} from 'react-bootstrap'
 import logo from './logo.svg'
 import './App.css'
 
@@ -9,14 +9,14 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">React Service App</h1>
-				</header>
+					<h1 className="App-title"> React Service App </h1>{' '}
+				</header>{' '}
 				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+					To get started, edit <code> src / App.js </code> and save to reload.{' '}
+				</p>{' '}
 				<p>
-					<Button bsStyle="primary">Yippee! A Primary Button</Button>
-				</p>
+					<Button bsStyle="primary"> Yippee!A Primary Button </Button>{' '}
+				</p>{' '}
 			</div>
 		)
 	}
